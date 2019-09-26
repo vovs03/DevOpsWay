6120769047b33a8b3aaa6a2dc5cea64933fd40ae
Vladimir Pavlychev <vladimir.pavlychev@osarts.ru>
2019-09-26 06:26:02 +0300

    Add: from laptop asus: variant(serpukhov) app use Classes

---
+++ App/src/tasks/task_001/traffic-light-6.rb
@@ -0,0 +1,24 @@
+# traffic-light-6.rb
+
+
+ligth(color)
+
+class Lamp
+
+	def light_on
+		puts light(color)
+	end
+
+    def light_off
+    	
+    end
+
+    def job_time
+		sleep 5    	
+    end
+
+
+
+end
+
+
